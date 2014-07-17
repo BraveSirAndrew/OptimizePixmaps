618aec1a212e8da5c63f66f8fc5a1cfdf0a3ef85
Andrew O'Connor <andrew@batcatgames.com>
2014-07-17 13:21:59 +0100

    First commit.

---
+++ OptimizePixmaps.Tests/OptimizePixmapsTests.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Linq;
+using Duality;
+using Duality.Resources;
+using NUnit.Framework;
+
+namespace OptimizePixmaps.Tests
+{
+	[TestFixture]
+    public class OptimizePixmapsTests
+    {
+		[Test]
+		public void RemovesFirstLayerOfAllPixmapsInPixmapsFolder()
+		{
+			if (Directory.Exists("Data\\Pixmaps") == false)
+			{
+				Directory.CreateDirectory("Data\\Pixmaps");
+			}
+
+			var layer = new Pixmap.Layer();
+			layer.SetPixelDataRgba(Enumerable.Repeat(MathF.Rnd.NextByte(), 400).ToArray(), 10, 10);
+			var pixmap = new Pixmap(layer);
+			pixmap.PixelData.Add(layer);
+			pixmap.Save("Data\\Pixmaps\\PixmapOne.pixmap.res");
+
+			Program.Main();
+
+			Assert.AreEqual(0, layer.Data.Length);
+		}
+
+		[Test]
+		public void DoesntRemoveSubsequentLayers()
+		{
+			if (Directory.Exists("Data\\Pixmaps") == false)
+			{
+				Directory.CreateDirectory("Data\\Pixmaps");
+			}
+
+			if(File.Exists("Data\\Pixmaps\\PixmapOne.pixmap.res"))
+				File.Delete("Data\\Pixmaps\\PixmapOne.pixmap.res");
+
+			var layer = new Pixmap.Layer();
+			layer.SetPixelDataRgba(Enumerable.Repeat(MathF.Rnd.NextByte(), 400).ToArray(), 10, 10);
+			var pixmap = new Pixmap(layer);
+			pixmap.PixelData.Add(layer);
+
+			var layer2 = new Pixmap.Layer();
+			layer2.SetPixelDataRgba(Enumerable.Repeat(MathF.Rnd.NextByte(), 400).ToArray(), 10, 10);
+			pixmap.PixelData.Add(layer2);
+
+			pixmap.Save("Data\\Pixmaps\\PixmapOne.pixmap.res");
+
+			Program.Main();
+
+			Assert.AreEqual(100, layer2.Data.Length);
+		}
+    }
+}
---
+++ OptimizePixmaps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Duality;
+using Duality.Resources;
+
+namespace OptimizePixmaps
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Optimizing pixmaps...");
+
+			var pixmapResources = Resource.GetResourceFiles("Data\\Pixmaps");
+			foreach (var pixmapResource in pixmapResources)
+			{
+				if (pixmapResource.EndsWith(".Pixmap.res", StringComparison.CurrentCultureIgnoreCase) == false)
+					continue;
+
+				var pixmap = ContentProvider.RequestContent<Pixmap>(pixmapResource);
+
+				if (pixmap == null)
+					continue;
+
+				pixmap.Res.PixelData[0].ClearPixelData();
+				pixmap.Res.Save();
+
+				ContentProvider.ClearContent();
+			}
+
+			Console.WriteLine("Finished");
+		}
+	}
+}
