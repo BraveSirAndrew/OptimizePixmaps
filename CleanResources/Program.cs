9afd62ed4abc363747688408731cbfa2ae189aeb
Andrea <roundcrisis@gmail.com>
2014-08-01 19:36:56 +0100

    Adding the clean resources project and nuget restore

--- CleanResources/Program.cs
+++ CleanResources/Program.cs
@@ -9,26 +9,55 @@ namespace CleanResources
 {
 	public class Program
 	{
+		private static string _pathToGame;
+
 		/// <summary>
 		/// Resets the local leaderboard and achievement as well as 
 		/// </summary>
 		/// <param name="args">pathToGame:string = Path to the game directory </param>
 		static void Main(string[] args)
 		{
-			string pathToGame = args[0];
-			if (string.IsNullOrEmpty(pathToGame))
+			_pathToGame = args[0];
+			if (string.IsNullOrEmpty(_pathToGame))
 			{
 				Console.WriteLine("Path to game is null or empty");
 				return ;
 			}
-			if(!Directory.Exists(pathToGame) )
+			if(!Directory.Exists(_pathToGame) )
 			{
-				Console.WriteLine("The directory {0} does not exist, can't reset leaderboars and tutorials",pathToGame);
+				Console.WriteLine("The directory {0} does not exist, can't reset leaderboars and tutorials",_pathToGame);
 				return;
 			}
-			CleanResource.ChangeTutorialCompleted(pathToGame);
-			CleanResource.ClearLeaderboardsAndAchivements(pathToGame);
+			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+			
+			Console.WriteLine("Succesfully loaded CorePluggin");
+			
+			CleanResource.ChangeTutorialCompleted(_pathToGame);
+			CleanResource.ClearLeaderboardsAndAchivements(_pathToGame);
+
+			Console.WriteLine("Finished reseting resources");
+			
 		}
+
+		private static Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			const string coreDll = "gameplugin.core";
+			var pluginPath = Path.Combine(_pathToGame, "plugins", coreDll + ".dll");
+			if (!File.Exists(pluginPath))
+			{
+				Console.WriteLine("Can't find Core Plugin, abort resource clean up");
+				throw new DllNotFoundException(string.Format("{0} not found, cant reset resources",pluginPath));
+			}
+
+			if (args.RequestingAssembly.FullName.ToLower().Contains(coreDll))
+			{
+				return Assembly.LoadFile(pluginPath);
+			}
+			return null;
+
+		}
+
+
 	}
 
 	public class CleanResource
@@ -39,8 +68,10 @@ namespace CleanResources
 			var content = LoadResource<PlayerProgressionResource>(pathToGame, playerProgressionResource.Path);
 			if (content == null)
 				return;
+			Console.WriteLine("Loaded player progression resource");
 			content.Res.TutorialCompleted = false;
 			content.Res.Save();
+			Console.WriteLine("Completed reseting to tutorial to not completed");
 		}
 
 		public static void ClearLeaderboardsAndAchivements(string pathToGame)
@@ -49,9 +80,13 @@ namespace CleanResources
 			var content = LoadResource<LocalPlatformResource>(pathToGame, leaderboard.Path);
 			if (content == null) 
 				return;
+			Console.WriteLine("Loaded platform resource");
+
 			content.Res.LeaderboardInfo.Clear();
 			content.Res.AchivementsUnlocked.Clear();
 			content.Res.Save();
+			Console.WriteLine("Completed reseting local leaderboards and achivements");
+
 		}
 
 		private static ContentRef<T> LoadResource<T>(string pathToGame,string pathToResource) where T : Resource
