058038fbd181b0f9b15caa5f963bf9a4c365b2d2
Andrea <roundcrisis@gmail.com>
2014-08-06 21:30:31 +0100

    Adding BuildAllScripts

---
+++ BuildAllScripts/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Duality;
+using ScriptingPlugin.Resources;
+
+namespace BuildAllScripts
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string folderPath = "Data\\Scripts";
+			var scriptResources = Resource.GetResourceFiles(folderPath);
+		
+			Fuck.Fucjk(scriptResources.ToArray());
+		}
+	}
+
+	public class Fuck
+	{
+		public static void Fucjk(string[] scripts)
+		{
+			Console.WriteLine("Optimizing pixmaps...");
+			if (scripts == null || scripts.Length == 0)
+			{
+				Console.WriteLine("Found 0 pixmaps.");
+				Console.WriteLine("Finished.");
+				return;
+			}
+			try
+			{
+
+				foreach (var scriptPath in scripts)
+				{
+					if (scriptPath.EndsWith("Script.res", StringComparison.CurrentCultureIgnoreCase) == false)
+						continue;
+					
+
+					if (scriptPath.EndsWith("FSharpScript.res"))
+					{
+						var scriptResource = ContentProvider.RequestContent<FSharpScript>(scriptPath);
+						if (scriptResource == null || scriptResource.Res == null)
+							continue;
+//						scriptResource.Res.Script
+//						scriptResource.Res.SourcePath
+
+					}
+					if (scriptPath.EndsWith("CSharpScript.res"))
+					{
+						var scriptResource = ContentProvider.RequestContent<CSharpScript>(scriptPath);
+
+					}
+					
+
+
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine("An error ocurred: {0} {1} {2}", exception.Message, Environment.NewLine, exception.StackTrace);
+			}
+			Console.WriteLine("Finished building scripts");
+
+		}
+	}
+}
--- CleanResources/Program.cs
+++ CleanResources/Program.cs
@@ -116,5 +116,6 @@ namespace CleanResources
 			}
 			return content;
 		}
+
 	}
 }
