cd645a4cd0a42d1f6e9051906eb792181fb46efa
Andrew O'Connor <bravesirandrew@gmail.com>
2015-08-01 18:53:54 +0100

    Fix OptimizePixmaps tests.

--- OptimizePixmaps.Tests/OptimizePixmapsTests.cs
+++ OptimizePixmaps.Tests/OptimizePixmapsTests.cs
@@ -38,7 +38,7 @@ namespace OptimizePixmaps.Tests
 			Program.Main();
 
 			var loadedPixmap = ContentProvider.RequestContent<Pixmap>(path).Res;
-			Assert.AreEqual(100, loadedPixmap.PixelData[1].ImageSize);
+			Assert.AreEqual(400, loadedPixmap.PixelData[1].ImageSize);
 		}
 
 		[Test]
